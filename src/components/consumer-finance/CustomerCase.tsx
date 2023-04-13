0a5346022295717752d1e24176c4ec5eb751a9a6
xingxiaoying <15050628@suning.com>
2023-04-13 20:31:06 +0800

    1111111

--- src/components/consumer-finance/CustomerCase.tsx
+++ src/components/consumer-finance/CustomerCase.tsx
@@ -25,11 +25,25 @@ const Card = styled.div`
         border-radius: 8px;
         background: rgba(255, 255, 255, 1);
         box-shadow: 0px 0px 24px 1px rgba(36, 91, 219, 0.12);
-    img{
+        overflow: hidden;
+    img.bg{
         height: 160px;
+        border-radius: 8px 8px 0 0;
+        &:hover{
+            transform: scale(1.02);
+            transition: transform 0.4s;
+        }
+    }
+    img.logo{
+        width: 100px;
+        height: 32px;
+        margin-top: 12px;
+        margin-left: 24px;
     }
     .textt_area{
-        
+        padding: 16px 24px;
+        font-size: 16px;
+        line-height: 26px;
     }
    }
 `
@@ -38,16 +52,20 @@ const Card = styled.div`
 const CustomerCaseList = [
     {
         title:'贷前、贷中、贷后全链路运营服务，助力小米信贷业务增长',
-        imgUrl: '/case-bg.png'
+        imgUrl: '/case-bg.png',
+        enterLogo: '/logo-1.png'
     },{
         title:'用AI和数据赋能业务增长，授信率、放款额等关键指标提升40%',
-        imgUrl: '/case-bg.png'
+        imgUrl: '/case-bg.png',
+        enterLogo: '/logo-1.png'
     },{
         title:'助力电销业务提质增效，实现效率效能双增长',
-        imgUrl: '/case-bg.png'
+        imgUrl: '/case-bg.png',
+        enterLogo: '/logo-1.png'
     },{
         title:'提供智能用户运营平台，推动招联消金私域业务人效提升100%',
-        imgUrl: '/case-bg.png'
+        imgUrl: '/case-bg.png',
+        enterLogo: '/logo-1.png'
     }
 ]
 const MainWrap = styled.div`
@@ -81,6 +99,7 @@ const CustomerCase: FC<SceneSolution> = ({}) => {
                 controller.destroy();
             });
     }, []);
+
     return (
         <Pane title="客户案例" titleStyle={{marginBottom:'56px'}} paneBgColor="white" style={{ paddingBottom: 100 }} mobileStyle={{ paddingBottom: 40 }}>
             <MainWrap id={AISOLUTION}>
@@ -88,7 +107,8 @@ const CustomerCase: FC<SceneSolution> = ({}) => {
                     <Card>
                         {CustomerCaseList.map(item => 
                             <div className='carditem'>
-                                <img src={imgurl+item.imgUrl} />
+                                <img  className="bg" src={imgurl+item.imgUrl} />
+                                <img className="logo" src={imgurl+item.enterLogo} />
                                 <div className='textt_area'>
                                     {item.title}
                                 </div>
