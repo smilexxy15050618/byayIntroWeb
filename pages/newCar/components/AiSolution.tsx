60956a0a72c2db86133d8060314bd3fdb62d43b1
Your Name <you@example.com>
2023-05-07 10:47:19 +0800

    汽车解决方案

--- pages/newCar/components/AiSolution.tsx
+++ pages/newCar/components/AiSolution.tsx
@@ -1,11 +1,12 @@
 import { CSSProperties, FC, ReactNode, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+
 import Pane from '../../../src/components/homepage/version2023/Pane';
 import { Hidden, Visible } from 'react-grid-system';
 import imgurl from '../../../img.url.js'
 // import { Swiper, SwiperSlide } from 'swiper/react';
 // import SwiperClass from 'swiper/types/swiper-class';
-interface  CoreValues{
+interface  SceneSolution{
 
 }
 const Wrapper = styled.div`
@@ -20,14 +21,17 @@ const Card = styled.div<{bg:number}>`
    margin:0 auto;
    display:flex;
    justify-content: space-between;
-   flex-wrap: wrap;
    .carditem{
     width: 284px;
-height: 234.19px;
-opacity: 1;
-border-radius: 8px;
-background: rgba(43, 88, 249, 1);
+    height: 240px;
+    opacity: 1;
+    border-radius: 8px;
+    background: rgba(43, 88, 249, 1);
     position: relative;
+    transition: all 0.3s ease-in;
+    &:hover{
+        transform: translateY(-8px);
+    }
     img{
         position: absolute;
         right: 0;
@@ -37,47 +41,48 @@ background: rgba(43, 88, 249, 1);
         position: absolute;
         width: 181px;
         height: 112px;
-        left:24px;
-        top:92px;
+        left:22px;
+        bottom:22px;
         .title_area{
             font-size: 20px;
-font-weight: 600;
-line-height: 24px;
-color: rgba(255, 255, 255, 1);
-margin-bottom:16px;
+            font-weight: 600;
+            line-height: 24px;
+            color: rgba(255, 255, 255, 1);
+            margin-bottom:16px;
         }
         .cont_area{
             font-size: 14px;
-font-weight: 400;
-letter-spacing: 0px;
-line-height: 24px;
-color: rgba(255, 255, 255, 1);
+            font-weight: 400;
+            letter-spacing: 0px;
+            line-height: 24px;
+            color: rgba(255, 255, 255, 1);
         }
     }
    }
     /* border-radius: 8px; */
     /* box-shadow: 0px 0px 24px 1px rgba(36, 91, 219, 0.12); */
-    
 `
 
 
-const carouselDataList = [{
-    title:'全渠道触点管理',
-    content:'每个触点回流后导向下一次触点判断，提升再触达精准度和用户转化效率。',
-    url: '/car_value1.png'
-},{
-    title:'旅程自动化运营',
-    content:'优化服务流程，有效降低人工时间成本和运营压力，助力企业营销提质增效。',
-    url: '/car_value2.png'
-},{
-    title:'数字化全景运营',
-    content:'不断拓宽自动化SOP的全场景应用边界，为企业构建并持续迭代精细化运营体系。',
-    url: '/car_value3.png'
-},{
-    title:'私域联动多维触达',
-    content:'为企业设计阶段性增长解决方案，优化沟通体验，提升用户的全生命周期价值。',
-    url: '/car_value4.png'
-}]
+const carouselDataList = [
+    {
+        title:'全渠道触点管理',
+        content:'每个触点回流后导向下一次触点判断，提升再触达精准度和用户转化效率',
+        url: '/use-bank-01.png'
+    },{
+        title:'私域联动多维触达',
+        content:'为企业设计阶段性增长解决方案，优化沟通体验，提升用户的全生命周期价值',
+        url: '/use-bank-02.png'
+    },{
+        title:'旅程自动化运营',
+        content:'优化服务流程，有效降低人工时间成本和运营压力，助力企业营销提质增效',
+        url: '/use-bank-03.png'
+    },{
+        title:'数字化全景运营',
+        content:'拓宽自动化SOP的全场景应用边界，为企业构建并持续迭代精细化运营体系',
+        url: '/use-bank-04.png'
+    }
+]
 const MainWrap = styled.div`
     transform: translateY(50%);
     transition: all 0.4s;
@@ -89,7 +94,7 @@ const MainWrap = styled.div`
 `
 
 const AISOLUTION = 'ai_jiejue_2918'
-export const CoreValues: FC<CoreValues> = ({}) => {
+const AiSolution: FC<SceneSolution> = ({}) => {
  
 
 const [wrapindex, setWrapindex] = useState(0);
@@ -119,7 +124,7 @@ useEffect(() => {
           <Hidden xs sm>
             <MainWrap id={AISOLUTION}>
             <HoveUp>
-                <Card bg={1}>
+                <Card>
                     {carouselDataList.map(item => 
                         <div className='carditem'>
                           <img src={imgurl+item.url} width={117}/>
@@ -133,15 +138,13 @@ useEffect(() => {
                           </div>
                       </div>
                     )}
-                  
                 </Card>
             </HoveUp>
             </MainWrap>
           </Hidden>
-          <Visible xs sm>
-            {/* <CarouselMobile dataList={carouselDataList}></CarouselMobile> */}
-          </Visible>
         </Pane>
     );
   };
-
+  
+  
+export default AiSolution;
