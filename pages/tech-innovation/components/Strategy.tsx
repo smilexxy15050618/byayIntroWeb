99ef02c50d100a11da31cc7d341d778828273415
xingxiaoying <xiaoying15050618@126.com>
2023-05-21 23:49:06 +0800

    技术创新

---
+++ pages/tech-innovation/components/Strategy.tsx
@@ -0,0 +1,152 @@
+import React, { FC, useState, useEffect, useRef, useCallback, useMemo } from 'react';
+import styled from 'styled-components';
+import imgurl from '../../../img.url.js'
+
+const MainWrap = styled.div`
+    width: 1200px;
+    padding: 80px 0;
+    box-sizing:border-box;
+    margin: 0 auto;
+    @media(max-width: 768px) {
+        padding: 40px 0;
+        }
+    .title {
+        font-size: 40px;
+        font-weight: 500;
+        letter-spacing: 0px;
+        line-height: 48px;
+        color: rgba(26, 26, 26, 1);
+        text-align: center;
+        margin-bottom: 16px;
+    }
+    .sec-title{
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 32px;
+        color: rgba(51, 51, 51, 1);
+        margin-bottom: 56px;
+        text-align:center;
+    }
+}
+`
+
+const StrateWrap = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .left{
+        width: 432px;
+        .copilot{
+            font-size: 28px;
+            font-weight: 500;
+            line-height: 32px;
+            color: rgba(26, 26, 26, 1);
+        }
+        .copilot-sub{
+            padding: 8px 0 24px;
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 24px;
+            color: rgba(90, 90, 90, 1);
+        }
+        .copilot-tag{
+            span{
+                display: inline-block;
+                width: 136px;
+                font-size: 14px;
+                font-weight: 400;
+                line-height: 24px;
+                color: rgba(51, 51, 51, 1);
+                &::before{
+                    display: inline-block;
+                    content: '';
+                    width: 24px;
+                    height: 24px;
+                    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARzQklUCAgICHwIZIgAAAF7SURBVEiJvZfdkkQwEIVPZ/ehuJqg5rnwXMqwV/FSK3uRMYIkWrDnRhXh6z756UbgSjUS9FXON7Q0V+qh9Q8AIC0q7udod8Tw6mYIQxo1Jwg/WLUVCKX3OSeAANwNPgtdBDBmSJ/9PviotZFwsRig2upyKACQ6Na3ZvCV9ro0vBbw2eqh1bdBJ1mWm4xVW90OBWCfAwZ8qcXU+59pCdVIA74yWz1mSPIMesz8cZmshXdADPSzZYQMDJQGTPQIfzFknQOqGrk7daqRIrhvNepd6zbQ7Z51iWd1+uyd8EjoPphQTqtwAz8BfYN35pBEt4GfhAIAsYuCq8pEQpEUJKB/a16IVuZnoG+Hv4+9IzqotgbGPg4KTG2SKRJ31GCfkoKAaVVz7T6rqR/Doiz+Q9bvbAF7Hyd5xjoeY2VluwQD91nu6Di3zV70NuFD3eBJV8y5p7UFQmd1kmfreeEDUSMpyAcFOL8wwNyTBess9Z81EgAeA28CsY5OJmitP4zTwmyzAME6AAAAAElFTkSuQmCC');
+                    background-position: center;
+                    background-repeat: no-repeat;
+                    background-size: 15px;
+                    vertical-align: -6px;
+                }
+            }
+        }
+        .copilot-list{
+            padding-top: 44px;
+            .copilot-list-dom{
+                img{
+                    width: 159px;
+                    height: 21px;
+                }
+                div{
+                    padding-top: 8px;
+                    font-size: 14px;
+                    font-weight: 400;
+                    line-height: 24px;
+                    color: rgba(90, 90, 90, 1);
+                }
+            }
+        }
+    }
+    .right{
+        width: 660px;
+        height: 378px;
+        img{
+            width: 660px;
+            height: 378px;
+        }
+    }
+`
+
+const copilotTag = [
+    {name: '精准触达人群'},
+    {name: '策略自动生成'},
+    {name: '自动化运行 '},
+    {name: '数据验证'},
+]
+
+const copilotList = [
+    {
+        img: 'copilot-1.svg',
+        name: 'AI算法根据历史数据生成的客户模型标签，可精准圈选出目标人群；'
+    },
+    {
+        img: 'copilot-2.svg',
+        name: '输出策略搭建意图，系统自动匹配已有模版流程中相似度最高策略；'
+    },
+    {
+        img: 'copilot-3.svg',
+        name: '分析不同运营策略效果数据，根据prompt进行自动分析，得出结论。'
+    }
+]
+export const Strategy: FC = () => {
+    return (
+        <MainWrap>
+            <div className="title">策略智能</div>
+            <div className="sec-title">策略自动生成、数据验证、自动化运行，提升关键指标和运营效率</div>
+            <StrateWrap>
+                <div className="left">
+                    <div className="copilot">MA Copilot</div>
+                    <div className="copilot-sub">一键拓展思路，高效生成运营策略</div>
+                    <div className="copilot-tag">
+                        {copilotTag.map((item, i) => {
+                            return (
+                                <span>{item.name}</span>
+                            );
+                        })}
+                    </div>
+                    <div className='copilot-list'>
+                        {copilotList.map((item, i) => {
+                            return (
+                                <div className="copilot-list-dom">
+                                    <img src={'/static/img2023/' + item.img} />
+                                    <div className="title">{item.name}</div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="right"><img src='/static/img2023/join-wap.png' /></div>
+            </StrateWrap>
+        </MainWrap>
+    )
+}
---
+++ pages/tech-innovation/components/WhySelect.tsx
@@ -0,0 +1,192 @@
+import React, { FC, useState, useEffect, useRef, useCallback, useMemo } from 'react';
+import styled from 'styled-components';
+import imgurl from '../../../img.url.js'
+import { Swiper, SwiperSlide} from 'swiper/react';
+import SwiperClass from 'swiper/types/swiper-class';
+import { RightArrow, LefttArrow } from '@indata/icon-byai';
+import { Visible } from 'react-grid-system';
+import ByProgressSwiper from '../../../src/components/common/ByProgressSwiper';
+
+const dataList = [
+    {
+        url: '/why1.png',
+        url_m: '/why1_m.png',
+    },
+    // {
+    //     url: '/why2.png'
+    // }
+]
+const Wrapper = styled.div`
+padding-top: 80px;
+padding-bottom: 100px;
+@media(max-width: 768px) {
+    padding-top: 40px;
+    padding-bottom: 32px;
+}
+.custom-bar-wrapper , .swiper-counter{
+    display:none;
+}
+  .myswiper-container {
+    width: 1200px;
+    margin: 0 auto;
+    flex-wrap:wrap;
+    display: flex;
+    justify-content: center;
+    transform: translateY(20%);
+    transition: all 0.4s;
+    opacity: 0;
+    @media (max-width: 768px) {
+        width:100vw;
+    }
+    &.appear{
+      transform: translateY(0);
+      opacity: 1;
+    }
+    .swiper-wrapper {
+        width: 1100px !important;
+        text-align: center;
+        @media (max-width: 768px) {
+            width:100vw !important;
+            .swiper-slide {
+                padding-bottom: 0 !important;
+            }
+        }
+      
+    }
+    .swiper-container {
+        background: rgba(255, 255, 255, 1);
+    }
+    .title {
+        display: block;
+        width: 306px;
+        height: 24px;
+        margin: 0 auto 109px;
+    }
+    .content {
+        display: block;
+        width: 100%;
+        height: 100%;
+        margin: 0 auto;
+    }
+    .my-slide {
+        width: 1100px;
+        height: 640px;
+        opacity: 1;
+        border-radius: 8px;
+    @media (max-width: 768px) {
+        width: 100vw;
+        height: auto;
+        padding: 0 6px;
+        box-sizing: border-box;
+    }
+    }
+  }
+`
+const Title = styled.div`
+font-size: 40px;
+font-weight: 500;
+letter-spacing: 0px;
+line-height: 48px;
+color: rgba(26, 26, 26, 1);
+text-align: center;
+margin-bottom: 48px;
+@media (max-width: 768px) {
+    font-size: 24px;
+    font-weight: 500;
+    letter-spacing: 0px;
+    line-height: 40px;
+}
+`
+const ArrowClick = styled.div`
+    width: 40px;
+    height: 40px;
+    opacity: 1;
+    align-self:center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color:#fff;
+    img{
+      width:100%;
+      cursor:pointer;
+    }
+`
+const WHYSELECT = 'WHYSELECT'
+export const WhySelect: FC = () => {
+    const [currIndex, setCurrIndex] = useState(0);
+    useEffect(() => {
+        const ScrollMagic = require('scrollmagic');
+        var controller = new ScrollMagic.Controller();
+        const videoContent = document.getElementById(WHYSELECT);
+        new ScrollMagic.Scene({
+            triggerElement: videoContent, //触发元素
+            triggerHook: 'onEnter', //触发元素开始离开视口时触发
+            offset: 10, //从开始点滚动多少px触发（施法前摇）
+            duration: 400, //效果持续的距离（法术持续时间/距离）
+        })
+            // .setClassToggle('.aitxs', 'appear')
+            .addTo(controller)
+            .on('enter', () => {
+                videoContent.classList.add('appear')
+                // console.log('进入');
+
+                controller.destroy();
+            });
+    }, []);
+    return (
+        <Wrapper>
+      <Visible md lg xl xxl xxxl>
+
+            <Title>客户为什么选择百应</Title>
+            <div id={WHYSELECT} className="myswiper-container">
+           
+            <Swiper effect="slide" autoplay={{delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,}} 
+            centeredSlides={true}
+            spaceBetween={20}
+            onSlideChange={swiper=>{
+                setCurrIndex(swiper.activeIndex)
+            }} style={{marginBottom:'42px'}}>
+                <SwiperSlide>
+                    <div className='my-slide'>
+                    <img className='content' src={imgurl+'/why1.png'} alt="" />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+            {/* <ArrowClick>
+            <img src={`${imgurl}/arowr1.png`}  />
+            </ArrowClick> */}
+
+            <Swiper effect="slide" autoplay={{delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,}} 
+            centeredSlides={true}
+            spaceBetween={20}
+            onSlideChange={swiper=>{
+                setCurrIndex(swiper.activeIndex)
+            }}>
+                <SwiperSlide>
+                    <div className='my-slide'>
+                    <img className='content' src={imgurl+'/why2.png'} alt="" />
+                    </div>
+                </SwiperSlide>
+            </Swiper>
+            </div>
+      </Visible>
+
+      <Visible xs sm>
+
+      <Title>客户为什么选择百应</Title>
+            <div id={WHYSELECT} className="myswiper-container">
+                    <div className='my-slide'>
+                    <img className='content' src={imgurl+'/why1_m.png'} alt="" />
+                    </div>
+                    <div style={{marginTop: '40px'}} className='my-slide'>
+                    <img className='content' src={imgurl+'/why2_m.png'} alt="" />
+                    </div>
+            </div>
+      </Visible>
+        </Wrapper>
+    )
+}
